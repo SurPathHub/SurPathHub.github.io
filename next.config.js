234070d7513811720cfe5933794891acd7381477
Vincent Villafuerte <73097560+vinzvinci@users.noreply.github.com>
2022-10-19 00:39:22 +0800

    fix: exportPathMap

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+    };
+  },
   reactStrictMode: true,
 }
 
